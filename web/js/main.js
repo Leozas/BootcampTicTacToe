69b2d6fc59b23b7bdc855bd0f00530ed67c761df
Michael Loyselle <52831149+Leozas@users.noreply.github.com>
2019-09-26 15:17:06 -0400

    UI Update .001 - pseudocode/bootstrap

--- web/js/main.js
+++ web/js/main.js
@@ -1,1 +1,53 @@
 //structure = var, then defined func, then overall func
+/*
+
+needs:
+=>ui
+-->gameboard
+-->btn
+-->Scores/PlayerTurn
+->rulesets
+-->wincondition
+-->visability rules
+-->player turn rules
+->resetbtn
+-->state
+--->clear, playing, someone won
+
+-->ui
+container
+-container-fluid
+--row
+---col
+---col
+---col
+--row
+---col
+---col
+---col
+--row
+---col
+---col
+---col
+--row
+--row
+---col-centered
+----row - turn
+----row - button 
+
+
+
+
+
+*/
+//createElement
+//setAttribute
+//appendChild
+
+// UI creation
+
+// atoms+molecules
+var createDiv = document.body.createElement("div")
+
+// gameboard organism
+document.body
